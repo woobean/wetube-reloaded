83953c8e5713bbf117b8415c11855cdd6220784e
wooobeen <woobin@xcast.kr>
2024-09-10 08:13:27 +0900

    6.4 Status Codesc

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   const videos = await Video.find({}).sort({ createdAt: "desc" });
-  return res.render("home", { pageTitle: "Home", videos });
+  return res.status(404).render("404", { pageTitle: "Video not found." });
 };
 
 export const watch = async (req, res) => {
@@ -28,7 +28,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.render("404", { pageTitle: "Video not found." });
+    return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -52,7 +52,7 @@ export const postUpload = async (req, res) => {
     });
     return res.redirect("/");
   } catch (error) {
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
