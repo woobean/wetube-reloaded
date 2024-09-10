4eeaa2a413c491d7d5622ee11f1afbd8617514b2
정우빈 <85533457+wooobeen@users.noreply.github.com>
2024-09-10 20:06:09 +0900

    17.2 Building For Production

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  avatarUrl: String,
+  avatarUrl: { type: String, default: "" },
   socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
   password: { type: String },
