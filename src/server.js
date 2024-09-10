2baa294a00723083f34091dca247b91065dab80f
wooobeen <woobin@xcast.kr>
2024-09-10 09:04:38 +0900

    6.14 Expiration and Secrets

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
-    secret: "Hello!",
+    secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
